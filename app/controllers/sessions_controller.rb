e51f7f966473c25a81a7879b1f38b1c0ac59737f
Adithya <kav.adithya@gmail.com>
2012-12-05 18:08:22 +0530

    1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
 	def new
 	end
 	def create
+    render :text => request.env['rack.auth'].inspect
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
