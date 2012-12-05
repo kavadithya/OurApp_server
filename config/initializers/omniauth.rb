1b341e135abf9a367eecd004f1ff2a639005a998
Adithya <kav.adithya@gmail.com>
2012-12-05 20:28:49 +0530

    1

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  
-  provider :facebook, '419059044832080', '0feebad62a0a48e811be55fcc252a0e1', :client_options => { :ssl => { :ca_file => "#{Rails.root}/config/ca-bundle.crt" } }
-  
+  if Rails.env == "development"
+  	provider :facebook, '419059044832080', '0feebad62a0a48e811be55fcc252a0e1', :client_options => { :ssl => { :ca_file => "#{Rails.root}/config/ca-bundle.crt" } }
+  elsif Rails.env == "production"
+	provider :facebook, '317364371704303', '98508ee8883ac7e5996f856be00ad3d7', :client_options => { :ssl => { :ca_file => "#{Rails.root}/config/ca-bundle.crt" } }
+  end
 end
